b99afee0d4bc9b3af48ff3ad885d801dfdd76899
disneyLadySango <sangota.d.duck@gmail.com>
2022-10-01 20:25:58 +0900

    lint: format

--- src/plugins/chakra-ui.ts
+++ src/plugins/chakra-ui.ts
@@ -1,13 +1,13 @@
-import ChakraUIVuePlugin, { chakra, extendTheme } from "@chakra-ui/vue-next";
-import { domElements } from "@chakra-ui/vue-system";
-import { defineNuxtPlugin, NuxtApp } from "nuxt/app";
+import ChakraUIVuePlugin, { chakra, extendTheme } from '@chakra-ui/vue-next'
+import { domElements } from '@chakra-ui/vue-system'
+import { defineNuxtPlugin, NuxtApp } from 'nuxt/app'
 
 export const plugin = (nuxtApp: NuxtApp) => {
-  const theme = extendTheme({});
-  nuxtApp.vueApp.use(ChakraUIVuePlugin, { ...theme });
-  domElements.forEach((tag) => {
-    nuxtApp.vueApp.component(`chakra.${tag}`, chakra(tag));
-  });
-};
+  const theme = extendTheme({})
+  nuxtApp.vueApp.use(ChakraUIVuePlugin, { ...theme })
+  domElements.forEach(tag => {
+    nuxtApp.vueApp.component(`chakra.${tag}`, chakra(tag))
+  })
+}
 
-export default defineNuxtPlugin(plugin);
+export default defineNuxtPlugin(plugin)
