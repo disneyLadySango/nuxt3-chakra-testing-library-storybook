2e5e4af5f79959193b7ca8633ead030a76c36959
disneyLadySango <sangota.d.duck@gmail.com>
2022-10-01 20:10:10 +0900

    feat: chakraの登録と仮実装を追加

---
+++ src/pages/test.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { CBox } from "@chakra-ui/vue-next";
+</script>
+<template>
+  <div>
+    <chakra.div p="8"> Hello World </chakra.div>
+    <CBox mt="auto">TEST</CBox>
+  </div>
+</template>
